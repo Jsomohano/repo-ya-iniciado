a2dbf8821d1970dec6d1042b332d1bc5d4a86b68
Jaime Somohano <jaimesomohano@gmail.com>
2025-07-09 21:48:49 -0600

    Resolviendo conflictos

--- Folder jaime/hola_mundo_jaime.py
+++ Folder jaime/hola_mundo_jaime.py
@@ -6,4 +6,5 @@ print("modificando el archivo hola_mundo_jaime.py")
 
 # Este es un comentario adicional para demostrar el uso de comentarios en Python.
 
+print("probando el merge")
 print("asdfasfasafds)
