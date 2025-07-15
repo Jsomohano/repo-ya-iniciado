b3b65bcc7fc9c3da130dd8a65e1f1f6db177e162
Jaime Somohano <jaimesomohano@gmail.com>
2025-07-14 20:23:10 -0600

    Solucionando errores del commit pasado

---
+++ Folder jaime/hola.py
@@ -0,0 +1,1 @@
+print("Solucionando otro bug")
--- Folder jaime/hola_mundo_jaime.py
+++ Folder jaime/hola_mundo_jaime.py
@@ -5,10 +5,4 @@ print("modificando el archivo hola_mundo_jaime.py")
 # Este es un comentario adicional para demostrar el uso de comentarios en Python.
 
 print("probando el merge")
-print("asdfasfasafds)
       
-      asdfasd
-      asdfajkshdf
-      asdfjklhasfdkj
-      adfkjahdfkjhasdf
-      adfkjahdskfjhasd
